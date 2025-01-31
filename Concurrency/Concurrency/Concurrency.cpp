77defe05abf5f3ed5f2781b82bdb868e8903f7f7
Abhinandan <akmmbhmishra.mishra@gmail.com>
2025-01-31 15:41:00 +0530

    increaseing no of thread usage

--- Concurrency/Concurrency/Concurrency.cpp
+++ Concurrency/Concurrency/Concurrency.cpp
@@ -9,9 +9,10 @@
 #include<thread>
 #include<cmath>
 #include<limits>
+#include<span>
 #include<mutex>
-constexpr size_t DATASET_SIZE = 5000000; 
-void ProcessDataset(std::array<int, DATASET_SIZE>& set , int &sum,std::mutex&mtx)
+constexpr size_t DATASET_SIZE = 50000000; 
+void ProcessDataset(std::span<int, DATASET_SIZE> set , int &sum,std::mutex&mtx)
 {
     for (int x : set)
     {
@@ -25,20 +26,30 @@ void ProcessDataset(std::array<int, DATASET_SIZE>& set , int &sum,std::mutex&mtx
         constexpr auto limit = (double)std::numeric_limits<int>::max();
         const auto y = (double)x / limit;
         
-        sum= int(std::sin(std::cos(y)) * limit);
+        sum+= int(std::sin(std::cos(y)) * limit);
        // mtx.unlock();
     }
 }
-int main()
-{
+std::vector<std::array<int, DATASET_SIZE>> GenerateDatasets() {
     std::minstd_rand rne;
     std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
-    std::vector<std::thread> workers;
+    
 
     for (auto& arr : datasets) {
         std::ranges::generate(arr, rne);
     }
-    int sum[] = { 0,0,0,0, };
+    return datasets;
+}
+int DoBiggie()
+{
+    auto datasets = GenerateDatasets();
+  std::vector<std::thread> workers;
+    struct Value
+    {
+        int v = 0;
+        char padding[64];
+    };
+    Value sum[4];
 
     std::mutex mtx;
     for (size_t i =0;i<4 ;i++)
@@ -51,7 +62,7 @@ int main()
                 set[0] += int(std::sin(std::cos(y)) * limit);
             }
             };*/
-        workers.push_back(std::thread{ ProcessDataset, std::ref(datasets[i]),std::ref(sum[i]),std::ref(mtx)});
+        workers.push_back(std::thread{ ProcessDataset, std::span(datasets[i]),std::ref(sum[i].v),std::ref(mtx)});
         // we cannot pass by reference directy if we have to pass by reference in thread
         // we have to create a wrapper of reference around it;
 
@@ -65,5 +76,44 @@ int main()
     }
     //join ak tarekhe se wait karta ahi threaed ko compelte hone ka
     // for multithreading we have some work to multithread
-    std::cout << "Hello World!\n"<<sum[0]+ sum[1]+ sum[2]+ sum[3];
+    std::cout << "Hello World!\n"<<sum[0].v+ sum[1].v+ sum[2].v+ sum[3].v;
+}
+int DoSmallies() {
+    auto datasets = GenerateDatasets();
+    std::vector<std::jthread> workers;
+    struct Value
+    {
+        int v = 0;
+        char padding[64];
+    };
+    Value sum[4];
+    int grandTotal = 0;
+    std::mutex mtx;
+    constexpr const auto subsetSize = DATASET_SIZE / 10000;
+    for(size_t i = 0;i < DATASET_SIZE;i+=subsetSize)
+    {
+        for (int j = 0;j < 4;j++)
+        {
+            workers.push_back(std::jthread{ ProcessDataset, std::span{&datasets[j][i],subsetSize},std::ref(sum[j].v),std::ref(mtx)});
+        }
+        workers.clear();
+        //when we clear then threads get distroyed and jthread distructor will be called 
+        // we do not have to join
+        grandTotal = sum[0].v + sum[1].v + sum[2].v + sum[3].v;
+    }
+    return 0;
+}
+int main(int argc, char** argv) {
+    if (argc > 1 && std::string{ argv[1] } == "--smol")
+    {
+        return DoSmallies();
+        //when we are breaking into parts 
+        // and we are increasing the no of parts they are breaking 
+        // the time of excution get increases as we increases the parts we are dividing
+        //reaason for this is we are creating a lots of thread and creating thread is expensive operation
+        //it call the operating system and it hast to create the context of the thread
+        // that include the allocating the memory for that thread stack every every thread
+         
+    }
+    return DoBiggie();
 }
